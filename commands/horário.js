170c3bad9218145f2d620bcb526a0ec33c416864
LorranAA <60102130+LorranAA@users.noreply.github.com>
2020-01-20 11:16:06 -0300

    Update horário.js

--- commands/horário.js
+++ commands/horário.js
@@ -28,7 +28,7 @@ exports.run = (client,message,args) => {
           },
           {
             "name": "✅  **Segunda**",
-            "value": "**21Hrs até 00Hrs**",
+            "value": "**20Hrs até 00Hrs**",
             "inline": true
           },
           {
