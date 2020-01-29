4cdb123ceb5e5809b06abcaa77f6054209d1898b
ThiagoTA <thiago.teixeirapf@gmail.com>
2020-01-29 20:25:39 -0300

    Update sugestão.js

--- commands/sugestão.js
+++ commands/sugestão.js
@@ -18,7 +18,7 @@ module.exports.run = (client, message, args) => {
         .setColor("BLUE")
         .setTitle(`${message.author.username}`)
         .setThumbnail(`${message.author.avatarURL}`)
-        .addField(`**${sugestão}**`)
+        .setDescription(`**${sugestão}**`)
         .setFooter(`Sugestão feita por ${message.author.username}`)
         .setTimestamp();
 
